4d599f6d50718818509990ae349b8ea474c67b31
Shweta-Git27 <65285048+Shweta-Git27@users.noreply.github.com>
2021-06-14 21:49:06 +0530

    Add files via upload

---
+++ Python/dwldyoutube.py
@@ -0,0 +1,11 @@
+import pafy
+
+url="https://www.youtube.com/watch?v=i_NmPv5WQ0U&list=RDi_NmPv5WQ0U&start_radio=1"
+
+video=pafy.new(url)
+
+best=video.getbest()
+
+best = video.getbest(preftype="mp4")
+
+filename = best.download(filepath="Videos") 
